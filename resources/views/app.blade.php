9d806e1d34fb07a3bef37fba34410deccfaf6172
guidoDomingo <ruizbenitezguido11@gmail.com>
2023-10-09 15:46:23 -0300

    actualizado 09/10/23 15:46

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -175,8 +175,11 @@
                         @yield('content')
 
                         <div class="d-flex justify-content-end" style="position: fixed; bottom: 20px; right: 20px;">
-                            <a id="back-to-top" href="#" class="btn bg-orange back-to-top" role="button" title="Volver arriba" data-bs-toggle="tooltip" data-bs-placement="right" style="width: 50px">
-                                <span class="glyphicon glyphicon-chevron-up"></span>
+                            <a id="back-to-top" href="#" class="btn bg-orange back-to-top" role="button" title="Volver arriba" data-bs-toggle="tooltip" data-bs-placement="right" style="text-align: center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-up" viewBox="0 0 16 16">
+                                    <path d="M7.646 1.854a.5.5 0 0 1 .708 0l5 5a.5.5 0 0 1-.708.708L8 2.207 2.354 7.854a.5.5 0 1 1-.708-.708l5-5z"/>
+                                    <path d="M7.5 2a.5.5 0 0 1 .5.5V13a.5.5 0 0 1-1 0V2.5a.5.5 0 0 1 .5-.5z"/>
+                                </svg>
                             </a>
                         </div>
 
